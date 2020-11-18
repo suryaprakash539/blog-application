1b7557f34c06a1af93af80ccd751d332917c5c75
suryaprakash <surya.zvky@gmail.com>
2020-11-18 11:21:14 +0530

    each author with their list of posts displayed

--- src/components/AuthorShow.js
+++ src/components/AuthorShow.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const AuthorShow = () => {
   const [author, setAuthor] = useState("");
+  const [posts, setPosts] = useState([]);
   const id = Number(useParams().id);
   useEffect(() => {
     axios
@@ -12,14 +13,35 @@ const AuthorShow = () => {
         setAuthor(response.data);
       });
   }, [id]);
-  console.log(author);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+      .then((response) => {
+        setPosts(response.data);
+      });
+  }, [id]);
+
+  //console.log(author);
   return (
     <div>
-      <h1>Name- {author.name}</h1>
-      <h2>Email- {author.email}</h2>
-      <h3>
-        Address- {author.address.street},{author.address.city}
-      </h3>
+      {author ? (
+        <div>
+          <h1>Name- {author.name}</h1>
+          <h2>Email- {author.email}</h2>
+          <h3>
+            Address- {author.address.street},{author.address.city}
+          </h3>
+          <h1>Listing posts written</h1>
+          <ul>
+            {posts.map((post) => (
+              <li>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
